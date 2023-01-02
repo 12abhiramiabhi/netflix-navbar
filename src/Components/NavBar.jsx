547945de74a4b24d94e210a1bb76ac68ccf0cade
abhirami <abhiabhirami972@gmail.com>
2023-01-02 13:07:06 +0530

    started netflix clone project

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <div>NavBar</div>
+        <div>
+            <div className='logo'>
+                {/* <img className='img' src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="" /> */}
+            </div>
+            <div className='black'>
+                <div className='red'>
+                    <div className='white'>
+                        <ul className='list'>
+                            <li>Home</li>
+                            <li>series</li>
+                            <li>movies</li>
+                            <li>New and popular</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 export default NavBar
