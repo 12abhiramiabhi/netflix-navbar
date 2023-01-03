e4de5940410d8eac43e1f3e81cb8113b9222b0a0
abhirami <abhiabhirami972@gmail.com>
2023-01-03 06:52:39 +0530

    completed navbar css

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <nav>
         <div>
+            <nav>
             <div className='logo'>
          <img className='img' src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" />
             </div>
             <div className='black'>
                 <div className='red'>
                     <div className='white'>
-                        <ul className='list'>
-                            <li>Home</li>
-                            <li>series</li>
-                            <li>movies</li>
-                            <li>New and popular</li>
-                        </ul>
+                        <div className='list'>
+                            <a href="#home"><li>Home</li></a>
+                            <a href="#series"><li>series</li></a>
+                            <a href="#movies"><li>movies</li></a>
+                            <a href="#new and popular"><li>New and popular</li></a>
+                        </div>
                     </div>
                 </div>
 
             </div>
-        </div>
     </nav>
+        </div>
     )
 }
 export default NavBar
