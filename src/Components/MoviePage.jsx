e4acc77c3e579d7d57bbd4d4fb015a0ba3aa19fb
abhirami <abhiabhirami972@gmail.com>
2023-01-15 20:19:13 +0530

    no changes

--- src/Components/MoviePage.jsx
+++ src/Components/MoviePage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { trandingURL } from './Constants';
 
 function MoviePage() {
     const location = useLocation();
     console.log(location.state);
+
+    const [allmovie,setallmovie]=useState({})
+
+    const MovieApi = async ()=>{
+        let response = await  axios.get(trandingURL)
+        console.log(response.data.result[0]);
+        setallmovie(response.data.result[9])
+    }
+
+    useEffect(()=>{
+        MovieApi()
+    },[])
+
     return (
         <div>
             <h1 className='page-head' style={{ color: "white" }}>
